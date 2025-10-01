5f0f7b110c3cf8be4a81454eaec0d8ca80ec6f8d
michellvalencia <michellvalencia2605@gmail.com>
2025-10-01 16:11:13 -0500

    Update loan.py

--- CODE/loan.py
+++ CODE/loan.py
@@ -1,20 +1,22 @@
 # ================================
 # Clase Prestamo
 # ================================
+
+from datetime import timedelta
 class Prestamo:
     def __init__(self, usuario, libro, fecha_prestamo, dias_prestamo):
         self.__usuario = usuario
         self.__libro = libro
-        self.__fecha_prestamo = fecha_prestamo
+        self.__fecha_prestamo = fecha_prestamo.strftime('%Y-%m-%d')
         self.__dias_prestamo = dias_prestamo
         # Fecha de devolución se calcula
-        self.__fecha_devolucion = None
-
-    def get_libro(self):
-        return self.__libro
+        self.__fecha_devolucion = (fecha_prestamo + timedelta(days=dias_prestamo)).strftime('%Y-%m-%d')
 
     def get_usuario(self):
         return self.__usuario
+        
+    def get_libro(self):
+        return self.__libro
         
     def get_fecha_prestamo(self):
         return self.__fecha_prestamo
@@ -25,16 +27,11 @@ class Prestamo:
     def get_fecha_devolucion(self):
         return self.__fecha_devolucion
 
-    def set_fecha_devolucion(self, fecha):
-        self.__fecha_devolucion = fecha
-
     def mostrar(self):
         print(f"Libro: {self.__libro.get_titulo()}")
         print(f"Usuario: {self.__usuario.get_nombre()}")
-        print(f"Fecha de préstamo: {self.__fecha_prestamo}")
-        print(f"Días de préstamo: {self.__dias_prestamo}")
-        if self.__fecha_devolucion is None:
-            print("Fecha de devolución: No asignada aún.")
-        else:
-            print(f"Fecha de devolución: {self.__fecha_devolucion}")
+        print(f"Fecha préstamo: {self.__fecha_prestamo}")
+        print(f"Días préstamo: {self.__dias_prestamo}")
+        print(f"Fecha devolución: {self.__fecha_devolucion}")
         print("-" * 30)
+
