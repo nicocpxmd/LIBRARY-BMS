43b0abf28138af40884578ddc1c415d075600f21
juandavog <juanewdogu2019@gmail.com>
2025-10-01 14:56:01 -0500

    Update user.py

--- CODE/user.py
+++ CODE/user.py
@@ -1,13 +1,16 @@
 # ================================
 # Clase Usuario
 # ================================
+from loan import Prestamo
+
 class Usuario:
     def __init__(self, nombre, identificador, tipo):
         self.__nombre = nombre
         self.__identificador = identificador
         self.__tipo = tipo  # estudiante o profesor
         self.__prestamos = []  # libros prestados
-
+        self.__historial = []  # libros prestados lifetime
+        
     def get_nombre(self):
         return self.__nombre
 
@@ -22,7 +25,31 @@ class Usuario:
 
     def agregar_prestamo(self, prestamo):
         self.__prestamos.append(prestamo)
+        self.__historial.append(prestamo)
 
     def devolver_prestamo(self, prestamo):
         if prestamo in self.__prestamos:
             self.__prestamos.remove(prestamo)
+
+    def mostrar_todo(self):
+        print(f"ID: {self.__identificador}")
+        print(f"Nombre: {self.__nombre}")
+        print(f"Tipo: {self.__tipo}")
+        print("---- Libros prestados actualmente ----")
+        if self.__prestamos:
+            for p in self.__prestamos:
+                libro = p.get_libro()
+                print(f"- {libro.get_titulo()} (Código: {libro.get_codigo()})")
+        else:
+            print("Ninguno")
+
+        print("---- Historial de préstamos ----")
+        if self.__historial:
+            for p in self.__historial:
+                libro = p.get_libro()
+                print(f"- {libro.get_titulo()} (Código: {libro.get_codigo()})")
+        else:
+            print("No tiene historial")
+        print("-" * 30)    
+    
+
