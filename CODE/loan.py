58f81bc6dda86bf2323bd5fa6d4be1c643461a9b
michellvalencia <michellvalencia2605@gmail.com>
2025-10-01 15:59:49 -0500

    Update loan.py

--- CODE/loan.py
+++ CODE/loan.py
@@ -15,3 +15,26 @@ class Prestamo:
 
     def get_usuario(self):
         return self.__usuario
+        
+    def get_fecha_prestamo(self):
+        return self.__fecha_prestamo
+
+    def get_dias_prestamo(self):
+        return self.__dias_prestamo
+
+    def get_fecha_devolucion(self):
+        return self.__fecha_devolucion
+
+    def set_fecha_devolucion(self, fecha):
+        self.__fecha_devolucion = fecha
+
+    def mostrar(self):
+        print(f"Libro: {self.__libro.get_titulo()}")
+        print(f"Usuario: {self.__usuario.get_nombre()}")
+        print(f"Fecha de préstamo: {self.__fecha_prestamo}")
+        print(f"Días de préstamo: {self.__dias_prestamo}")
+        if self.__fecha_devolucion is None:
+            print("Fecha de devolución: No asignada aún.")
+        else:
+            print(f"Fecha de devolución: {self.__fecha_devolucion}")
+        print("-" * 30)
