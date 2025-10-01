d29d791d208abdfed4d486d81b4d24c610c14b4c
juandavog <juanewdogu2019@gmail.com>
2025-10-01 14:29:19 -0500

    Update main.py

---
+++ CODE/main.py
@@ -0,0 +1,21 @@
+# ================================
+# Programa Principal
+# ================================
+if __name__ == "__main__":
+    # Aquí es donde creas los libros y usuarios "de inicio"
+    # Pista: cargar al menos 10 libros y 3 usuarios antes del menú.
+    # Ejemplo:
+    # biblio = Biblioteca()
+    # biblio.agregar_libro(Libro("Python Básico", "Autor1", 2020, "Programación", "L1", 3))
+    # biblio.agregar_usuario(Usuario("Juan", "U1", "estudiante"))
+
+    # Luego haces un menú en consola con while True:
+    """
+    1. Listar libros
+    2. Consultar libros
+    3. Realizar préstamo
+    4. Devolver libro
+    5. Consultar préstamos por usuario
+    6. Salir
+    """
+    # En cada opción llamas a los métodos de la clase Biblioteca.
